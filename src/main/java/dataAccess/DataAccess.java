931850aa1725f76bdb2a1574f98cbc2ceed813b7
aimarsanm <146017584+aimarsanm@users.noreply.github.com>
2024-10-03 08:51:27 +0200

    adaptability high

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -26,7 +26,7 @@ import exceptions.RideMustBeLaterThanTodayException;
 public class DataAccess {
 	private EntityManager db;
 	private EntityManagerFactory emf;
-
+	private static final String ACTION_1 = "Donostia"; 
 	ConfigXML c = ConfigXML.getInstance();
 	
 	private String adminPass="admin";
@@ -104,11 +104,11 @@ public class DataAccess {
 			cal.set(2024, Calendar.APRIL, 20);
 			Date date4 = UtilDate.trim(cal.getTime());
 
-			driver1.addRide("Donostia", "Madrid", date2, 5, 20); //ride1
-			driver1.addRide("Irun", "Donostia", date2, 5, 2); //ride2
-			driver1.addRide("Madrid", "Donostia", date3, 5, 5); //ride3
+			driver1.addRide(ACTION_1, "Madrid", date2, 5, 20); //ride1
+			driver1.addRide("Irun", ACTION_1, date2, 5, 2); //ride2
+			driver1.addRide("Madrid", ACTION_1, date3, 5, 5); //ride3
 			driver1.addRide("Barcelona", "Madrid", date4, 0, 10); //ride4
-			driver2.addRide("Donostia", "Hondarribi", date1, 5, 3); //ride5
+			driver2.addRide(ACTION_1, "Hondarribi", date1, 5, 3); //ride5
 
 			Ride ride1 = driver1.getCreatedRides().get(0);
 			Ride ride2 = driver1.getCreatedRides().get(1);
